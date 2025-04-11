4981dee7a8bb490e10cd571f7dcec4df301e83ef
maryamasif2008 <marayam.asif123@gmail.com>
2025-04-11 21:00:30 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,225 @@
+let myArray = [10, 20, 30, 40, 50];
+for (let i = 0; i <=6; i++) {
+    console.log(myArray[i]);
+}
+
+let alArray = ["car","bus","bike"];
+for (let i = 0; i <=6; i++) {
+    console.log(alArray[i]);
+}
+
+let numArray = [2, 4, 6, 8, 10];
+for (let i = 0; i <=6; i++) {
+    console.log(numArray[i]);
+}
+
+let yourArray = ["orange" ,"red" ,"black"];
+for (let i = 0; i <=6; i++) {
+    console.log(yourArray[i]);
+}
+
+let vegArray = ["tomato" ,"Potato" ,"piece"];
+for (let i = 0; i <=6; i++){
+    console.log(vegArray[i]);
+}
+
+let mArray = [10, 20, 30, 40, 50];
+for (let i = 0; i <=6; i++) {
+    console.log(myArray[i]);
+}
+
+let aArray = ["car","bus","bike"];
+for (let i = 0; i <=6; i++) {
+    console.log(alArray[i]);
+}
+
+let nArray = [2, 4, 6, 8, 10];
+for (let i = 0; i <=6; i++) {
+    console.log(numArray[i]);
+}
+
+let yArray = ["orange" ,"red" ,"black"];
+for (let i = 0; i <=6; i++) {
+    console.log(yourArray[i]);
+}
+
+let vArray = ["tomato" ,"Potato" ,"piece"];
+for (let i = 0; i <=6; i++){
+    console.log(vegArray[i]);
+}
+
+let iArray = [1 ,7 ,8 ,10 ,12];
+for (let i = 0; i <=6; i++){
+    console.log(iArray[i]);
+}
+
+let oArray = ["Mania" ,"Fasbha" ,"Maryam"];
+for (let i = 0; i <=6; i++){
+    console.log(oArray[i]);
+}
+
+let foodArray = ["pizza" , "burger" , "sandwich"];
+for (let i = 0; i <=6; i++){
+    console.log(foodArray[i]);
+}
+
+let carArray = ["corolla" ,"ody" ,"farari"];
+for (let i = 0; i <=6; i++){
+    console.log(carArray[i]);
+}
+
+let uArray = [3 ,4 ,6 ,7];
+for (let i = 0; i<=6; i++){
+    console.log(uArray[i]);
+}
+
+let zArray = ["study" , "stationary" , "books"];
+for (let i = 0; i<=6; i++){
+    console.log(zArray[i]);
+}
+
+let wArray = ["a" , "b" , "c", "d"];
+for (let i = 0; i<=6; i++){
+    console.log(wArray[i]);
+}
+
+let eArray = [33 ,44 ,66 ,77];
+for (let i = 0; i<=6; i++){
+    console.log(eArray[i]);
+}
+
+let xArray = [44 , 55 , 88 , 99];
+for (let i = 0; i<=6; i++){
+    console.log(xArray[i]);
+}
+
+let fArray = ["a" , "b" , "c", "d"];
+for(let i = 0; i<=6; i++){
+    console.log(fArray[i]);
+}
+
+let gArray = ["Mania" ,"Fasbha" ,"Maryam"];
+for (let i = 0; i <=6; i++){
+    console.log(gArray[i]);
+}
+
+let hArray = ["pizza" , "burger" , "sandwich"];
+for (let i = 0; i <=6; i++){
+    console.log(hArray[i]);
+}
+
+let qArray = ["corolla" ,"ody" ,"farari"];
+for (let i = 0; i <=6; i++){
+    console.log(qArray[i]);
+}
+
+let meArray = [10, 20, 30, 40, 50];
+for (let i = 0; i <=6; i++) {
+    console.log(meArray[i]);
+}
+
+let usArray = ["car","bus","bike"];
+for (let i = 0; i <=6; i++) {
+    console.log(alArray[i]);
+}
+
+let weArray = [2, 4, 6, 8, 10];
+for (let i = 0; i <=6; i++) {
+    console.log(weArray[i]);
+}
+
+let heArray = ["orange" ,"red" ,"black"];
+for (let i = 0; i <=6; i++) {
+    console.log(heArray[i]);
+}
+
+let sheArray = ["tomato" ,"Potato" ,"piece"];
+for (let i = 0; i <=6; i++){
+    console.log(sheArray[i]);
+}
+
+let reArray = ["corolla" ,"ody" ,"farari"];
+for (let i = 0; i <=6; i++){
+    console.log(reArray[i]);
+}
+
+let ueArray = [10, 20, 30, 40, 50];
+for (let i = 0; i <=6; i++) {
+    console.log(ueArray[i]);
+}
+
+let yeArray = ["a" , "e" , "i" ,"o" ,"u"];
+for (let i = 0; i<=6; i++){
+    console.log(yeArray[i]);
+}
+
+let zeArray = [10, 20, 30, 40, 50];
+for (let i = 0; i <=6; i++) {
+    console.log(zeArray[i]);
+}
+
+let geArray = ["a" , "e" , "i" ,"o" ,"u"];
+for (let i = 0; i<=6; i++){
+    console.log(geArray[i]);
+}
+
+let oeArray = [100, 200, 300, 400, 500];
+for (let i = 0; i <=6; i++) {
+    console.log(ueArray[i]);
+}
+
+let peArray = ["a" , "e" , "i" ,"o" ,"u"];
+for (let i = 0; i<=6; i++){
+    console.log(yeArray[i]);
+}
+
+let keArray = [11, 44 ,77 ,88];
+for (let i = 0; i<=6; i++){
+    console.log(keArray[i]);
+}
+
+let menArray = [ "my" , "us" , "we" , "he"];
+for (let i = 0; i<=6; i++){
+    console.log(menArray[i]);
+}
+
+let womenArray = [23 , 34 , 67 ,44];
+for (let i = 0; i<=6; i++){
+    console.log(womenArray[i]);
+}
+
+let humArray = ["father" , "mother" ,"brother"];
+for(let i = 0; i<=6; i++){
+    console.log(humArray[i]);
+}
+
+let neArray = ["a" , "e" , "i" ,"o" ,"u"];
+for (let i = 0; i<=6; i++){
+    console.log(neArray[i]);
+}
+
+let ieArray = [11, 44 ,77 ,88];
+for (let i = 0; i<=6; i++){
+    console.log(ieArray[i]);
+}
+
+let leArray = [ "my" , "us" , "we" , "he"];
+for (let i = 0; i<=6; i++){
+    console.log(leArray[i]);
+}
+
+let yuArray = [23 , 34 , 67 ,44];
+for (let i = 0; i<=6; i++){
+    console.log(yuArray[i]);
+}
+
+let tuArray = ["father" , "mother" ,"brother"];
+for(let i = 0; i<=6; i++){
+    console.log(tuArray[i]);
+}
+
+let loArray = [ 22 ,44 ,77];
+for(let i = 0; i <=6; i++){
+    console.log(loArray[i]);
+}
+
